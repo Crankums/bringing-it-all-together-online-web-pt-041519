27dae64767c749913e66a27c5d1256e82c5292a3
Craig Donavin <crdonavin@gmail.com>
2019-07-04 19:30:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -23,9 +23,9 @@ attr_accessor :id, :name, :breed
   end
 
   def save
-    # if self.id
-    #   self.update
-    # else
+    if self.id
+      self.update
+    else
     sql = <<-SQL
       INSERT INTO dogs (name, breed)
       VALUES (?, ?)

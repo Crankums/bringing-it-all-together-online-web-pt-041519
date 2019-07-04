e0c03d57a478d0c12551ff404f2ee17747a0d53a
Craig Donavin <crdonavin@gmail.com>
2019-07-04 18:52:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -49,6 +49,6 @@ attr_accessor :id, :name, :breed
       WHERE id = ?
     SQL
     result = DB[:conn].execute(sql, id).flatten
-    Dog.new(id, result[1], result[2])
+    Dog.new(id: result[0], name:result[1], breed:result[2])
   end
 end

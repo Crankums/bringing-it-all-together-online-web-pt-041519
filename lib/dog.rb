1e6d71fb215c9b4dadbee426db5d3daf54e259fb
Craig Donavin <crdonavin@gmail.com>
2019-07-04 18:38:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -21,4 +21,17 @@ attr_accessor :id, :name, :breed
   def self.drop_table
     DB[:conn].execute('DROP TABLE IF EXISTS dogs')
   end
+
+  def save
+    # if self.id
+    #   self.update
+    # else
+    sql <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?) 
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.album)
+    @id = DB[:conn].execute('SELECT last_insert_rowid() FROM dogs')[0][0]
+  end
 end
